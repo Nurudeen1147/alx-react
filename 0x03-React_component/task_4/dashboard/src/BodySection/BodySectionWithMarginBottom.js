de9428db20230735c5e14e710b91a9e09feba9e0
Nurudeen Oyelowo <neroevali4u@yahoo.co.uk>
2023-11-01 07:47:55 +0000

    Body Section

---
+++ 0x03-React_component/task_4/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BodySection from './BodySection';
+import './BodySectionWithMarginBottom.css';
+
+class BodySectionWithMarginBottom extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='bodySectionWithMargin'>
+        <BodySection {...this.props} />
+      </div>
+    );
+  }
+}
+
+BodySectionWithMarginBottom.defaultProps = {
+  title: '',
+};
